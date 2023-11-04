d7ca64c7ae76b1c60e15328f6aa508a6d3368573
SamyOffer <samy.offer@student.vinci.be>
2023-11-04 16:06:56 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,139 @@
+import Logo from "../assets/logo2.png";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonFill } from "react-icons/bs";
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import CV from "../assets/CV_OFFER_SAMY_06.pdf";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <Link to="home" smooth={true} duration={500} >
+      <img className="cursor-pointer" src={Logo} alt="LogoImage" style={{ width: "50px" }} />
+      </Link>
+
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? (
+          <FaBars className="text-3xl" />
+        ) : (
+          <FaTimes className="text-3xl" />
+        )}
+      </div>
+      {/* Mobile Menu*/}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl ">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          {/* Linkedin */}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/samy-offer-38a559293/"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          {/* Github */}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/SamyOffer"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          {/* Mail */}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#7C5D63]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:samyoffer@gmail.com"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          {/* Resume */}
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href={CV}
+              download="CV_OFFER_SAMY_06.pdf"
+            >
+              Resume <BsFillPersonFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import TodoWebSite from '../assets/projects/TodoWebSite.png'
+import Ressourcerie from '../assets/projects/ressourcerie.png'
+import WebSitePortfolio from '../assets/projects/WebSitePortfolio.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Web Site Portfolio",
+        image:WebSitePortfolio,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Ressourcerie Web Site",
+        image:Ressourcerie,
+        github:"https://github.com/SamyOffer/Ressourcerie",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Todo JS Application",
+        image:TodoWebSite,
+        github:"",
+        live:"",
+    }
+
+
+]
